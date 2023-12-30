2ec51721a7cfb641d04b4fd07afb3ba2723a58d1
mihir <mihirsarvan193@gmail.com>
2023-12-30 22:22:07 +0530

    Revert "1Flask Project"
    This reverts commit c65d5c524d34b2bac9a4a45689275a144547c429.

--- OneDrive/Desktop/FSDSP/Assignments/21st_October_Flask assignment/DB7.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-con =sqlite3.connect("sql.db")
-print("Data base Opened sucessfull")
-con.execute("create table student(Id int ,name Text ,address Text)")
-print("table created sucess fully")
-con.close()
